84943fea3f71a5bbef12e6730af1a68d0076c357
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-05-12 08:31:38 -0400

    move common setup event to constructor

--- src/main/java/rpg/project/pmmogating/PmmoGating.java
+++ src/main/java/rpg/project/pmmogating/PmmoGating.java
@@ -1,17 +1,24 @@
 package rpg.project.pmmogating;
 
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import rpg.project.pmmogating.setup.CommonSetup;
+import rpg.project.lib.api.gating.GateUtils;
+import rpg.project.pmmogating.core.EventConfigType;
+import rpg.project.pmmogating.core.EventGates;
+import rpg.project.pmmogating.core.ProgressionConfigType;
+import rpg.project.pmmogating.core.ProgressionGates;
 
 @Mod(PmmoGating.MODID)
 public class PmmoGating {
 	public static final String MODID = "pmmogating";
 	
 	public PmmoGating() {
-		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-		
-		modBus.addListener(CommonSetup::init);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+	}
+	
+	public void init(FMLCommonSetupEvent event) {
+		GateUtils.registerProgressGate(ProgressionConfigType.ID, ProgressionConfigType.IMPL, new ProgressionGates());
+		GateUtils.registerEventGate(EventConfigType.ID, EventConfigType.IMPL, new EventGates());
 	}
 }
