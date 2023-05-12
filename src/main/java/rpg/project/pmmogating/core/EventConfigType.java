49fbc0a829b792887aaa3686330cd311d43e0329
Caltinor <62700786+Caltinor@users.noreply.github.com>
2023-05-12 08:31:46 -0400

    setup event gates

---
+++ src/main/java/rpg/project/pmmogating/core/EventConfigType.java
@@ -0,0 +1,67 @@
+package rpg.project.pmmogating.core;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.resources.ResourceLocation;
+import rpg.project.lib.api.APIUtils;
+import rpg.project.lib.api.data.MergeableData;
+import rpg.project.lib.api.data.SubSystemConfig;
+import rpg.project.lib.api.data.SubSystemConfigType;
+import rpg.project.pmmogating.PmmoGating;
+
+public record EventConfigType() implements SubSystemConfigType{
+	public static final ResourceLocation ID = new ResourceLocation(PmmoGating.MODID, "event");
+	public static final EventConfigType IMPL = new EventConfigType();
+
+	@Override
+	public Codec<SubSystemConfig> getCodec() {
+		return EventConfig.CODEC;
+	}
+
+	public static record EventConfig(Map<String, List<SubSystemConfig>> reqs) implements SubSystemConfig {
+		public static final Codec<SubSystemConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+				Codec.unboundedMap(Codec.STRING, Codec.list(APIUtils.getDispatchCodec().dispatch("type", SubSystemConfig::getType, SubSystemConfigType::getCodec)))
+					.fieldOf("requirements")
+					.forGetter(ssc -> ((EventConfig)ssc).reqs())
+				).apply(instance, EventConfig::new));
+
+		@Override
+		public MergeableData combine(MergeableData other) {
+			EventConfig two = (EventConfig) other;
+			Map<String, List<SubSystemConfig>> current = new HashMap<>(this.reqs());
+			two.reqs().forEach((contiainer, list) -> {
+				current.merge(contiainer, list, (o, n) -> {
+					return Stream.concat(o.stream(), n.stream()).distinct().toList();
+				});
+			});
+			return new EventConfig(current);
+		}
+
+		@Override
+		public boolean isUnconfigured() {
+			return reqs().entrySet().stream().allMatch(entry -> entry.getValue().isEmpty());
+		}
+
+		@Override
+		public Codec<SubSystemConfig> getCodec() {
+			return CODEC;
+		}
+
+		@Override
+		public SubSystemConfig getDefault() {
+			return new EventConfig(new HashMap<>());
+		}
+
+		@Override
+		public SubSystemConfigType getType() {
+			return IMPL;
+		}
+		
+	}
+}
---
+++ src/main/java/rpg/project/pmmogating/core/EventGates.java
@@ -0,0 +1,41 @@
+package rpg.project.pmmogating.core;
+
+import java.util.List;
+import java.util.Map;
+
+import org.joml.Math;
+
+import net.minecraft.resources.ResourceLocation;
+import rpg.project.lib.api.Hub;
+import rpg.project.lib.api.data.ObjectType;
+import rpg.project.lib.api.data.SubSystemConfig;
+import rpg.project.lib.api.events.EventContext;
+import rpg.project.lib.api.gating.GateSystem;
+import rpg.project.lib.api.gating.GateUtils.Type;
+import rpg.project.lib.api.progression.ProgressionDataType;
+import rpg.project.lib.api.progression.ProgressionDataType.Comparison;
+import rpg.project.pmmogating.core.EventConfigType.EventConfig;
+
+public class EventGates implements GateSystem {
+
+	@Override
+	public float isActionPermitted(EventContext context, Hub core, ResourceLocation eventID, String ignored) {		
+		EventConfig eventGates = (EventConfig) core.getGateData(EventConfigType.IMPL, ObjectType.EVENT, Type.EVENT, eventID).orElse(null);
+		if (eventGates == null)
+			return 1f;
+		
+		float outValue = 1f;
+		for (Map.Entry<String, List<SubSystemConfig>> reqs : eventGates.reqs().entrySet()) {
+			ProgressionDataType eventGateValue = reqs.getValue().stream()
+					.filter(config -> config.getType().equals(core.getProgression().dataType()))
+					.map(ssc -> (ProgressionDataType)ssc)
+					.findFirst()
+					.orElse(null);
+			ProgressionDataType currentProgress = core.getProgression().getProgress(context.actor().getUUID(), reqs.getKey());
+			outValue = Math.min(outValue, eventGateValue == null ? 1f : currentProgress.compare(Comparison.GREATER_THAN_OR_EQUAL, eventGateValue));
+		}
+		
+		return outValue;
+	}
+
+}
